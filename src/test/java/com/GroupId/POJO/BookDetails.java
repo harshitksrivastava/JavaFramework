079252699f482f542ab0a5cb9352ebbb46516c9a
harshitksrivastava <harsh0311@gmail.com>
2024-12-29 03:16:31 +0530

    updated SheetName in annotation in POJO class

--- src/test/java/com/GroupId/POJO/BookDetails.java
+++ src/test/java/com/GroupId/POJO/BookDetails.java
@@ -4,7 +4,7 @@ import com.poiji.annotation.ExcelCell;
 import com.poiji.annotation.ExcelCellName;
 import com.poiji.annotation.ExcelSheet;
 
-@ExcelSheet("sheetName")
+@ExcelSheet("ProgrammingBooks")
 public class BookDetails {
 	
 	@ExcelCell(0)
